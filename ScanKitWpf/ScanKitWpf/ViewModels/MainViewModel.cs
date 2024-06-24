e2fd03b47a887c706f2ba00184e514e243d7cb88
billy_lj <billy_lj@126.com>
2024-06-24 16:57:04 +0800

    传送识别到的原始信息

--- ScanKitWpf/ScanKitWpf/ViewModels/MainViewModel.cs
+++ ScanKitWpf/ScanKitWpf/ViewModels/MainViewModel.cs
@@ -157,7 +157,8 @@ namespace ScanKitWpf.ViewModels
                 {
                     OneGrab();
                     AddMsg($"解析耗时：{sw.ElapsedMilliseconds}毫秒");
-                    var materialInfo = ParseCode();
+                    
+                    //var materialInfo = ParseCode();
                     var sendData = JsonSerializer.Serialize(codeInfos);
                     //var sendData = "{\"SN\":\"20240603000380\",\"PN\":\"2924011226\",\"Qty\":16000,\"Lot\":\"N/A\",\"DC\":\"2423\",\"Supplier\":\"70D050\",\"OtherBarcode\":\"2924011226{16000{PCE{70D050{2423{03{N/A{N/A{20240603000380\",\"RotAngle\":-34}";
                     var sendBytes = Encoding.UTF8.GetBytes(sendData);
