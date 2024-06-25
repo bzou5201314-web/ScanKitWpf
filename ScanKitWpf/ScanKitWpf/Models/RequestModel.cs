a1115859efaf1a3b36f520a9bdfafa0db80d4a24
billy_lj <billy_lj@126.com>
2024-06-25 14:16:12 +0800

    提交确实的class

---
+++ ScanKitWpf/ScanKitWpf/Models/RequestModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScanKitWpf.Models
+{
+    public class RequestModel<T>
+    {
+        public string MsgType { get; set; }
+
+        public T Data { get; set; }
+    }
+
+    public class ReelSize
+    {
+        public int MatSize { get; set; }
+    }
+}
