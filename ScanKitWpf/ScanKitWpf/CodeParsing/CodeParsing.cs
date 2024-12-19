c6d62e5a5cb43903be31be136a88c0e820e91495
billy_lj <billy_lj@126.com>
2024-12-19 11:21:30 +0800

    解析逻辑拆分到单独Class文件中

---
+++ ScanKitWpf/ScanKitWpf/CodeParsing/CodeParsing.cs
@@ -0,0 +1,17 @@
+﻿using ScanKitWpf.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScanKitWpf
+{
+    public class CodeParsing
+    {
+        public static MaterialInfoModel ParseCode(IList<CodeInfo> codeInfos)
+        {
+            return new MaterialInfoModel();
+        }
+    }
+}
