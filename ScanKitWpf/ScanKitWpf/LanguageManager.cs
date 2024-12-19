f0fcc3a688d37321ea4af44ee959ef3050be6d6a
billy_lj <billy_lj@126.com>
2024-12-19 11:22:22 +0800

    添加语言管理类

---
+++ ScanKitWpf/ScanKitWpf/LanguageManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ScanKitWpf
+{
+    public static class LanguageManager
+    {
+        public static void SetLanguage(string languageCode)
+        {
+            var rd = new ResourceDictionary();
+            rd.Source = new Uri($"language/Resource.{languageCode}.xaml", UriKind.Relative);
+            for (int i=0; i<Application.Current.Resources.MergedDictionaries.Count; i++)
+            {
+                var item= Application.Current.Resources.MergedDictionaries[i];
+                if (item.Source != null && item.Source.OriginalString.Contains("language"))
+                {
+                    Application.Current.Resources.MergedDictionaries.Remove(item);
+                }
+            }
+            Application.Current.Resources.MergedDictionaries.Add(rd);
+        }
+    }
+}
